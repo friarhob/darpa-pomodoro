8266a960a37a48653a5199a615aa0890d0ac634b
Friar Hob <github@friarhob.33mail.com>
2018-10-25 18:11:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock.js';
 
 class App extends Component {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      delayMinutes: 50,
+      currentTime: new Date().getTime(),
+      startTime: new Date().getTime(),
+      endTime: new Date().getTime()
+    };
+
+    this.offsetDelayMinutes = this.offsetDelayMinutes.bind(this);
+    this.updateDelayMinutes = this.updateDelayMinutes.bind(this);
+  }
+
+  pad(number)
+  {
+    var stringNumber = String(number);
+    if(stringNumber.length < 2) stringNumber = "0"+stringNumber;
+    return stringNumber;
+  }
+
+  offsetDelayMinutes(amount)
+  {
+    this.setState({
+      delayMinutes: (parseInt(this.state.delayMinutes)+amount)
+    });
+  }
+
+  updateDelayMinutes(newTime)
+  {
+    this.setState({
+      delayMinutes: newTime
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>DARPA</h1>
+          <h2>DARPA&apos;s Another React-based Pomodoro App</h2>
         </header>
+        <main>
+          <Clock />
+          <a href="#" className="up" onClick={() => this.offsetDelayMinutes(1)}>up</a>
+          <input type="text" value={this.state.delayMinutes} onChange={(e) => this.updateDelayMinutes(e.target.value)}></input>
+          <a href="#" className="down" onClick={() => this.offsetDelayMinutes(-1)}>down</a>
+
+        </main>
+        <footer>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/friarhob">Friar Hob</a> wishes you all to have a great life!
+        </footer>
       </div>
     );
   }
---
+++ src/Clock.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      currentTime: new Date().getTime(),
+    };
+
+    var x = setInterval(() => {
+      this.setState({currentTime: new Date().getTime()})
+    }, 500);
+
+  }
+
+  pad(number)
+  {
+    var stringNumber = String(number);
+    if(stringNumber.length < 2) stringNumber = "0"+stringNumber;
+    return stringNumber;
+  }
+
+  render() {
+    const time = Math.floor((this.state.currentTime%(24*60*60*1000))/1000);
+    return (
+      <div className="Clock">
+          <p>{this.pad(Math.floor(time/(60*60)))}:{this.pad(Math.floor((time%(60*60))/60))}:{this.pad(Math.floor(time%(60)))} (UTC)</p>
+      </div>
+    );
+  }
+}
+
+export default Clock;
