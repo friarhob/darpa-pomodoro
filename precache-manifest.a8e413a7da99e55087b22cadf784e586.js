f7e103e69a00c2a9f199b4a5b7cc1c8849ef6a66
Friar Hob <github@friarhob.33mail.com>
2018-10-26 15:20:33 -0300

    Updates

---
+++ precache-manifest.a8e413a7da99e55087b22cadf784e586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef2b4234436f66adb96",
+    "url": "/darpa-pomodoro/static/js/runtime~main.2ef2b423.js"
+  },
+  {
+    "revision": "61916bcef6f43f30b778",
+    "url": "/darpa-pomodoro/static/js/main.61916bce.chunk.js"
+  },
+  {
+    "revision": "fd97677c7b9454c5e64f",
+    "url": "/darpa-pomodoro/static/js/1.fd97677c.chunk.js"
+  },
+  {
+    "revision": "61916bcef6f43f30b778",
+    "url": "/darpa-pomodoro/static/css/main.def97d8c.chunk.css"
+  },
+  {
+    "revision": "89d6a555ad05b661dbae7a32da26ed8b",
+    "url": "/darpa-pomodoro/index.html"
+  }
+];
