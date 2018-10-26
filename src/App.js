56071bc824490e4a70b049a61cbedd259ca5ed8c
Friar Hob <github@friarhob.33mail.com>
2018-10-26 01:16:57 -0300

    Updating favicon.ico Adding proper counter on App (still not as a separate component)

--- src/App.js
+++ src/App.js
@@ -11,15 +11,31 @@ class App extends Component {
     this.state = {
       sessionMinutes: 50,
       breakMinutes: 10,
-      currentTime: new Date().getTime(),
-      startTime: new Date().getTime(),
-      endTime: new Date().getTime()
+      running: {
+        STOP: 0,
+        WORK: 1,
+        PLAY: 2,
+        LONG: 3,
+        current: 0,
+        properties: [
+          {label: "Work", title: "Click Button to Start"},
+          {label: "Stop", title: "Work Time"},
+          {label: "Stop", title: "Play Time"},
+          {label: "Stop", title: "Long Rest"}
+        ],
+        intervalFunction: null
+      },
+      startTime: 0,
+      endTime: 0,
+      remainingTime: 0
     };
 
     this.offsetSessionMinutes = this.offsetSessionMinutes.bind(this);
     this.updateSessionMinutes = this.updateSessionMinutes.bind(this);
     this.offsetBreakMinutes = this.offsetBreakMinutes.bind(this);
     this.updateBreakMinutes = this.updateBreakMinutes.bind(this);
+
+    this.toggleButton = this.toggleButton.bind(this);
   }
 
   pad(number)
@@ -57,21 +73,56 @@ class App extends Component {
     });
   }
 
+  toggleButton(e)
+  {
+    e.preventDefault();
+    if(this.state.running.current == this.state.running.STOP)
+    {
+      this.state.running.current = this.state.running.PLAY;
+      this.state.running.intervalFunction = setInterval(() => {
+        if(new Date().getTime() > this.state.endTime) {
+          this.setState({
+            endTime: (new Date().getTime())+((this.state.running.current == this.state.running.WORK)?this.state.breakMinutes:this.state.sessionMinutes)*60*1000
+          });
+          this.state.running.current = 3-this.state.running.current;
+        }
+        this.setState({
+          remainingTime: (this.state.endTime - new Date().getTime())
+        });
+      }, 200);
+    }
+    else
+    {
+      this.state.running.current = this.state.running.STOP;
+      clearInterval(this.state.running.intervalFunction);
+      this.state.running.intervalFunction = null;
+      this.setState({
+        endTime: 0,
+        remainingTime: 0
+      });
+    }
+  }
+
   render() {
+    const time = (this.state.remainingTime%(24*60*60*1000))/1000;
     return (
       <div className="App">
         <header>
           <h1>DARPA</h1>
           <h2>DARPA&apos;s Another React-based Pomodoro App</h2>
+
+          <Clock />
         </header>
         <main>
-          <Clock />
-
-          <TimeLength label='Session time (in minutes)' minutes={this.state.sessionMinutes} offset={this.offsetSessionMinutes} update={this.updateSessionMinutes} />
-          <TimeLength label='Break time (in minutes)' minutes={this.state.breakMinutes} offset={this.offsetBreakMinutes} update={this.updateBreakMinutes} />
-
-          
-
+          <nav>
+            <TimeLength label='Work time (in minutes)' minutes={this.state.sessionMinutes} offset={this.offsetSessionMinutes} update={this.updateSessionMinutes} />
+            <TimeLength label='Play time (in minutes)' minutes={this.state.breakMinutes} offset={this.offsetBreakMinutes} update={this.updateBreakMinutes} />
+          </nav>
+          <article>
+            <h1>{this.state.running.properties[this.state.running.current].title}</h1>
+            <p>{this.pad(Math.floor(time/(60*60)))}:{this.pad(Math.floor((time%(60*60))/60))}:{this.pad(Math.floor(time%(60)))}</p>
+            <button className="toggle" onClick={(e) => this.toggleButton(e)}>{this.state.running.properties[this.state.running.current].label}</button>
+          </article>
         </main>
         <footer>
           <a target="_blank" rel="noopener noreferrer" href="https://github.com/friarhob">Friar Hob</a> wishes you all to have a great life!
